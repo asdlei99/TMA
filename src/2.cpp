07601d2fa9279bfdd2cc3a511d9d7545ab6b5ebc
yifuda <xin_soft@foxmail.com>
2018-12-12 22:10:01 +0800

    加速，我知道这样违反了游戏规则。

--- src/2.cpp
+++ src/2.cpp
@@ -1,13 +1,20 @@
 #include "main.h"
 
+float mandelbrot(int i, int j) {
+	float x=0,y=0;int k;for(k=0;k++<256;){float a=x*x-y*y+(i-768.0)/512;y=2*x*y+(j-512.0)/512;x=a;if(x*x+y*y>4)break;}return log(k);
+}
+
+static float m;
+
 unsigned char RD(int i,int j){
-float x=0,y=0;int k;for(k=0;k++<256;){float a=x*x-y*y+(i-768.0)/512;y=2*x*y+(j-512.0)/512;x=a;if(x*x+y*y>4)break;}return log(k)*47;
+	m = mandelbrot(i, j);
+	return m * 47;
 }
 
 unsigned char GR(int i,int j){
-float x=0,y=0;int k;for(k=0;k++<256;){float a=x*x-y*y+(i-768.0)/512;y=2*x*y+(j-512.0)/512;x=a;if(x*x+y*y>4)break;}return log(k)*47;
+	return m * 47;
 }
 
 unsigned char BL(int i,int j){
-float x=0,y=0;int k;for(k=0;k++<256;){float a=x*x-y*y+(i-768.0)/512;y=2*x*y+(j-512.0)/512;x=a;if(x*x+y*y>4)break;}return 128-log(k)*23;
+	return 128 - m * 23;
 }
